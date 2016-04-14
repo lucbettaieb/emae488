bc2ba298c02c0a96306b5c926fbe32b6f5edb054
Luc Bettaieb <luc.bettaieb@gmail.com>
2016-04-14 14:40:19 -0400

    Inward iterations working.

--- hw4/p1/hw3_p1.m
+++ hw4/p1/hw3_p1.m
@@ -25,20 +25,35 @@ R_inv_pion4 = [ [cos(pi/4), sin(pi/4),  0]
                 [-sin(pi/4),cos(pi/4),  0]
                 [0,         0,          1]];
 
-omega = zeros(1, n_links);
-omega_dot = zeros(1, n_links);
+omega = zeros(3, n_links);
+omega_dot = zeros(3, n_links);
 
-v = zeros(1, n_links);
-v_com_dot = zeros(1, n_links);
+v = zeros(3, n_links);
+v_com_dot = zeros(3, n_links);
 
-f_vec = zeros(1, n_links);
+F_vec = zeros(3, n_links);
+N_vec = zeros(3, n_links);
+
+% Inital conditions
+omega(:,1) = [0,0,0]';
+omega_dot(:,1) = [0,0,0]';
+
+v(:,1) = [0,0,0]';
+v_com_dot(:,1) = [0,0,0]';
+
+F_vec(:,1) = m_i * v_com_dot(1);
+N_vec(:,1) = [0,0,0]';
 
 % Outward iterations
-for i = 1:n_links
-    omega(i) = 
-    omega_dot(i) = 
-    v(i) = 
-    v_com_dot(i) = 
+for i = 2:n_links+1
+    omega(:,i) = [0,0,0]';
+    omega_dot(:,i) = [0,0,0]';
+    
+    v(:,i) = R_fwd_pion4*(cross(omega_dot(:,i-1), p_c_i) + cross(omega(:,i-1), cross(omega(:,i-1), p_c_i)) + v_g_i );
+    v_com_dot(:,i) = cross(omega_dot(:,i), p_c_i) +  cross(omega(:,i), cross(omega(:,i), p_c_i)) + v(:,i);
+   
+    F_vec(:,i) = m_i * v_com_dot(:,i);
+    N_vec(:,i) = [0,0,0]';
 end
 
-
+% Inward iterations
